f21009d7cbe3eef93f3e8a5830f8598d86a992cf
nkhaccuong <cuong19012002@gmail.com>
2025-04-10 16:23:24 +0700

    Add fetch account api for user route

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-
+import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   const configService = app.get(ConfigService);
   const port = configService.get("PORT");
   const urlUserVice = configService.get<string>("USER_SERVICE_URL");
