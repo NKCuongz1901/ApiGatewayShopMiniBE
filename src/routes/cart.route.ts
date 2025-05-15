39623be1c1985831570867335ca16f2e958b40ef
moonthien <letrongthien23032003@gmail.com>
2025-05-15 20:50:39 +0700

    done add order

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,5 +1,5 @@
 import { HttpService } from "@nestjs/axios";
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { lastValueFrom } from "rxjs";
 
@@ -17,7 +17,28 @@ export class CartGatewayController {
     async addToCart(@Body() body: { userId: string, productId: string, quantity: number }) {
         try {
             const { data } = await lastValueFrom(
-                this.httpService.post(`${this.cartServiceUrl}`, body),
+                this.httpService.post(`${this.cartServiceUrl}`, body,{
+                    headers: {
+                        'x-internal-api-key': process.env.INTERNAL_API_KEY || 'my-secret-key', // thêm header
+                    },
+                } )
+            );
+            return data;
+        } catch (error) {
+            throw new HttpException(error.response?.data || 'Error add to cart', HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+    @Patch('update')
+    async updateCart(@Body() body: { userId: string, productId: string, quantity: number }) {
+        try {
+            const { data } = await lastValueFrom(
+                this.httpService.patch(`${this.cartServiceUrl}/update`, body,{
+                    headers: {
+                        'x-internal-api-key': process.env.INTERNAL_API_KEY || 'my-secret-key', // thêm header
+                    },
+                } )
             );
             return data;
         } catch (error) {
@@ -43,4 +64,42 @@ export class CartGatewayController {
         }
     }
 
+    //remove cart
+    @Delete('remove')
+    async removeCart(@Body() body: { userId: string}) {
+        try {
+            const { data } = await lastValueFrom(
+                this.httpService.delete(`${this.cartServiceUrl}/remove` ,{
+                    headers: {
+                        'x-internal-api-key': process.env.INTERNAL_API_KEY || 'my-secret-key', // thêm header
+                    },
+                    data: body,
+                }, 
+            )
+            );
+            return data;
+        } catch (error) {
+            throw new HttpException(error.response?.data || 'Error remove cart', HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+    //remove product in cart
+    @Delete('remove-product')
+    async removeProductInCart(@Body() body: { userId: string, productId: string }) {
+        try {
+            const { data } = await lastValueFrom(
+                this.httpService.delete(`${this.cartServiceUrl}/remove-product`, {
+                    headers: {
+                        'x-internal-api-key': process.env.INTERNAL_API_KEY || 'my-secret-key', // thêm header
+                    },
+                    data: body,
+                })
+            );
+            return data;
+        } catch (error) {
+            throw new HttpException(error.response?.data || 'Error remove product in cart', HttpStatus.BAD_REQUEST);
+
+        }
+    }
 }
